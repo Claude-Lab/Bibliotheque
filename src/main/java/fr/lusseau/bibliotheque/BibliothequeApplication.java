68ddbbccd01e4b6412231e973a89ebf2cf9e1012
Claude LUSSEAU <claude.lusseau@yahoo.fr>
2020-09-01 10:58:18 +0200

    Test

--- src/main/java/fr/lusseau/bibliotheque/BibliothequeApplication.java
+++ src/main/java/fr/lusseau/bibliotheque/BibliothequeApplication.java
@@ -2,12 +2,25 @@ package fr.lusseau.bibliotheque;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"fr.lusseau.bibliotheque"})
 public class BibliothequeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliothequeApplication.class, args);
 	}
+	
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        registry.viewResolver(resolver);
+    }
 
 }
--- src/test/java/fr/lusseau/bibliotheque/BibliothequeApplicationTests.java
+++ src/test/java/fr/lusseau/bibliotheque/BibliothequeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bibliotheque;
+package fr.lusseau.bibliotheque;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
