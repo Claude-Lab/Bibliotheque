bee210a2b50417d1f6601904accb08142a15ed74
Claude LUSSEAU <claude.lusseau@yahoo.fr>
2020-09-02 21:38:56 +0200

    Create StringToDate.java

---
+++ src/main/java/fr/lusseau/bibliotheque/configuration/StringToDate.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package fr.lusseau.bibliotheque.configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * Classe en charge de la convertion des String en Date/Time.
+ * @Version Bibliotheque -v1,0
+ * @date  2 sept. 2020 - 11:10:36
+ * @author Claude LUSSEAU
+ *
+ */
+public class StringToDate implements Converter<String, LocalDateTime> {
+
+
+	/**
+	 * @{inheritDoc}
+	*/
+	@Override
+	public LocalDateTime convert(String source) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy HH:mm", Locale.FRANCE);
+        LocalDateTime date = LocalDateTime.parse(source, formatter);
+		return date;
+	}
+}
