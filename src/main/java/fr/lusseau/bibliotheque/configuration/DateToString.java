a540393c4335de19f46307e47c5adf75a7d3c08b
Claude LUSSEAU <claude.lusseau@yahoo.fr>
2020-09-02 21:35:47 +0200

    Update DateToString

---
+++ src/main/java/fr/lusseau/bibliotheque/configuration/DateToString.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package fr.lusseau.bibliotheque.configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.core.convert.converter.Converter;
+
+/**
+ * Classe en charge de la convertion des dates en String.
+ * 
+ * @Version Bibliotheque -v1,0
+ * @date 2 sept. 2020 - 10:35:20
+ * @author Claude LUSSEAU
+ *
+ */
+public class DateToString implements Converter<LocalDateTime, String>{
+	
+	
+
+	/**
+	 * @{inheritDoc}
+	*/
+	@Override
+	public String convert(LocalDateTime source) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		String formatDateTime = source.format(formatter);
+		return formatDateTime;
+	}
+
+}
